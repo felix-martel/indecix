c01a254d85206895010dda7ca0425ec03aa3c0f2
felix <felix.martel@polytechnique.edu>
2016-10-14 14:47:16 +0200

    Chgt type='submit' en type='button' et modif activation lien page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-console.log("init ok");
 /*$('#container').bind('swipeleft', goLeft);
 $('#container').bind('swiperight', goRight);*/
 
@@ -17,11 +16,17 @@ function goLeft() {
 
 function goRight() {}
 
+function activateTitles(e){
+    console.log("Activation...");
+    $('.menu-item-a').removeClass("active");
+    $(e).addClass("active");
+}
+/*
 $(".menu-item-a").click(function() {
     console.log("active nav link ok");
     $('.menu-item-a').removeClass("active");
     $(this).addClass("active");
-});
+});*/
 
 serverURL = '';
 
@@ -29,7 +34,6 @@ serverURL = '';
 function login() {
     var username = $("input[name='username']").val();
     var password = $("input[name='password']").val();
-    alert("Envoi des données");
     console.log("Envoi des données...");
     $.post(serverURL + 'login.php', {username: username, password: password}, 
         function(messageJson) {
@@ -53,27 +57,13 @@ function login() {
     window.location.replace("index.html#all");
 }
 
-function login2() {
-    $.ajax({
-      type:    "POST",
-      url:     "/"+contentId+"/postComment",
-      data:    {"postComment":""},
-      success: function(data) {
-            alert('call back');
-      },
-      // vvv---- This is the new bit
-      error:   function(jqXHR, textStatus, errorThrown) {
-            alert("Error, status = " + textStatus + ", " +
-                  "error thrown: " + errorThrown
-            );
-      }
-    });
-}
-
 function add_khote() {
     var khoteur = $("input[name='khoteur']").val();
     var khote = $("input[name='khote']").val();
-    $.post(serverURL + 'add.php', {khoteur: khoteur, khote: khote});
+    $.post(serverURL + 'add.php', 
+        {khoteur: khoteur, khote: khote, MODAL: sessionStorage['session_id']});
+    console.log("OK c'est parti");
+    window.location.replace("index.html#all");
 }
 
 $(window).on('hashchange', route);
@@ -81,19 +71,11 @@ function route() {
     var page, hash = window.location.hash;
     console.log(hash);
     switch (hash) {
-        /*case '#all':
-            $.get('js/template.html', function(templates) {
-                var template = $(templates).filter('#main-tpl').html();
-                page = Mustache.render(template, data);
-                $('#container').html(page);
-            }, 'html');
-            break;*/
         case '#all':
             $.get('js/template.html', function(templates) {
                 var template = $(templates).filter('#main-tpl').html();
                 $.getJSON(serverURL + "get.php?MODAL="+sessionStorage['session_id']+"&filter=all", 
                     function(data) {
-                    console.log("get OK");
                     page = Mustache.render(template, data);
                     $('#container').html(page);
                 });
@@ -105,7 +87,6 @@ function route() {
                 var template = $(templates).filter('#main-tpl').html();
                 $.getJSON(serverURL + "get.php?MODAL="+sessionStorage['session_id']+"&filter=top", 
                     function(data) {
-                    console.log("get OK");
                     page = Mustache.render(template, data);
                     $('#container').html(page);
                 });
@@ -117,7 +98,6 @@ function route() {
                 var template = $(templates).filter('#main-tpl').html();
                 $.getJSON(serverURL + "get.php?MODAL="+sessionStorage['session_id']+"&filter=fav", 
                     function(data) {
-                    console.log("get OK");
                     page = Mustache.render(template, data);
                     $('#container').html(page);
                 });
