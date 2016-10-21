8c0e9dceb2c94b596a28dd82368a24d3a500b531
Der Siebte Schatten <der-siebte-schatten@D-850>
2016-10-21 13:51:49 +0200

    Implementation de la fonction action(action, id)

--- js/index.js
+++ js/index.js
@@ -10,12 +10,12 @@ var page_href = ['#all', '#top', '#fav'];
 var nb_pages = 3;
 $('#container').bind('swipeleft', goLeft);
 $('#container').bind('swiperight', goRight);
-function setCurrentPage(n){
-    if (n >= 0 && n < nb_pages){
+function setCurrentPage(n) {
+    if (n >= 0 && n < nb_pages) {
         current_page = n;
-        n = n+1;
+        n = n + 1;
         $(".menu-item-a").removeClass("active");
-        $(".menu-item-a:nth-of-type("+n+")").addClass("active");
+        $(".menu-item-a:nth-of-type(" + n + ")").addClass("active");
     }
 }
 function goLeft() {
@@ -30,7 +30,7 @@ function goRight() {
     console.log("Going right...");
 }
 
-function activateTitles(e){
+function activateTitles(e) {
     console.log("Activation...");
     $('.menu-item-a').removeClass("active");
     $(e).addClass("active");
@@ -42,25 +42,24 @@ function login() {
     var username = $("input[name='username']").val();
     var password = $("input[name='password']").val();
     console.log("Envoi des données...");
-    $.post(serverURL + 'login.php', {username: username, password: password}, 
-        function(messageJson) {
-            console.log('lecture json');
-            var messageAffiche = "";
-            for (var i=0; i < messageJson.length; i++) {
-                if (messageJson[i].session_id) {
-                    sessionStorage['session_id'] = messageJson[i].session_id;
-                    console.log('session id =' + sessionStorage['session_id']);
+    $.post(serverURL + 'login.php', {username: username, password: password},
+            function (messageJson) {
+                console.log('lecture json');
+                var messageAffiche = "";
+                for (var i = 0; i < messageJson.length; i++) {
+                    if (messageJson[i].session_id) {
+                        sessionStorage['session_id'] = messageJson[i].session_id;
+                        console.log('session id =' + sessionStorage['session_id']);
+                    }
+                    if (messageJson[i].error) {
+                        messageAffiche = "Error : " + messageJson[i].error;
+                        alert(messageAffiche);
+                    } else if (messageJson[i].success) {
+                        console.log("Bienvenue");
+                        window.location.replace("index.html");
+                    }
                 }
-                if (messageJson[i].error) {
-                    messageAffiche = "Error : " + messageJson[i].error;
-                    alert(messageAffiche);
-                }
-                else if (messageJson[i].success) {
-                    console.log("Bienvenue");
-                    window.location.replace("index.html");
-                }
-            }
-        });
+            });
     window.location.replace("index.html#all");
 }
 
@@ -70,13 +69,25 @@ function add_khote() {
     console.log(new_khoteur);
     console.log(new_khote);
     $.post(
-        serverURL + "add.php",
-        {MODAL : sessionStorage['session_id'], khoteur : new_khoteur, khote: new_khote},
-        function(data) {
-        }
+            serverURL + "add.php",
+            {MODAL: sessionStorage['session_id'], khoteur: new_khoteur, khote: new_khote},
+            function (data) {
+            }
     );
     console.log("OK c'est parti");
     window.location.replace("index.html#all");
+}
+
+function action(action, id) {
+    alert(action + " sur khote " + id);
+    $.post(
+            serverURL + "action.php",
+            {MODAL: sessionStorage['session_id'], action: action, id: id},
+            function (data) {
+            }
+    );
+    console.log("Action effectuée");
+    window.location.reload();
 }
 
 $(window).on('hashchange', route);
@@ -85,49 +96,49 @@ function route() {
     console.log(hash);
     switch (hash) {
         case '#all':
-            $.get('js/template.html', function(templates) {
+            $.get('js/template.html', function (templates) {
                 var template = $(templates).filter('#main-tpl').html();
-                $.getJSON(serverURL + "get.php?MODAL="+sessionStorage['session_id']+"&filter=all", 
-                    function(data) {
-                        if (data === "not_logged_in") {
-                            window.location.replace("index.html#login");
-                        }
-                    page = Mustache.render(template, data);
-                    $('#container').html(page);
-                });
+                $.getJSON(serverURL + "get.php?MODAL=" + sessionStorage['session_id'] + "&filter=all",
+                        function (data) {
+                            if (data === "not_logged_in") {
+                                window.location.replace("index.html#login");
+                            }
+                            page = Mustache.render(template, data);
+                            $('#container').html(page);
+                        });
             }, 'html');
             break;
 
         case '#top':
-            $.get('js/template.html', function(templates) {
+            $.get('js/template.html', function (templates) {
                 var template = $(templates).filter('#main-tpl').html();
-                $.getJSON(serverURL + "get.php?MODAL="+sessionStorage['session_id']+"&filter=top", 
-                    function(data) {
-                        if (data === "not_logged_in") {
-                            window.location.replace("index.html#login");
-                        }
-                    page = Mustache.render(template, data);
-                    $('#container').html(page);
-                });
+                $.getJSON(serverURL + "get.php?MODAL=" + sessionStorage['session_id'] + "&filter=top",
+                        function (data) {
+                            if (data === "not_logged_in") {
+                                window.location.replace("index.html#login");
+                            }
+                            page = Mustache.render(template, data);
+                            $('#container').html(page);
+                        });
             }, 'html');
             break;
 
         case '#fav':
-            $.get('js/template.html', function(templates) {
+            $.get('js/template.html', function (templates) {
                 var template = $(templates).filter('#main-tpl').html();
-                $.getJSON(serverURL + "get.php?MODAL="+sessionStorage['session_id']+"&filter=fav", 
-                    function(data) {
-                        if (data === "not_logged_in") {
-                            window.location.replace("index.html#login");
-                        }
-                    page = Mustache.render(template, data);
-                    $('#container').html(page);
-                });
+                $.getJSON(serverURL + "get.php?MODAL=" + sessionStorage['session_id'] + "&filter=fav",
+                        function (data) {
+                            if (data === "not_logged_in") {
+                                window.location.replace("index.html#login");
+                            }
+                            page = Mustache.render(template, data);
+                            $('#container').html(page);
+                        });
             }, 'html');
             break;
 
         case '#new':
-            $.get('js/template.html', function(templates) {
+            $.get('js/template.html', function (templates) {
                 var template = $(templates).filter('#writing-page-tpl').html();
                 page = Mustache.render(template, {});
                 $('#container').html(page);
@@ -135,7 +146,7 @@ function route() {
             break;
 
         case '#login':
-            $.get('js/template.html', function(templates) {
+            $.get('js/template.html', function (templates) {
                 var template = $(templates).filter('#login-page-tpl').html();
                 page = Mustache.render(template, {});
                 $('#container').html(page);
@@ -143,17 +154,17 @@ function route() {
             break;
 
         default:
-            $.get('js/template.html', function(templates) {
+            $.get('js/template.html', function (templates) {
                 var template = $(templates).filter('#main-tpl').html();
-                $.getJSON(serverURL + "get.php?MODAL="+sessionStorage['session_id']+"&filter=all", 
-                    function(data) {
-                        if (data === "not_logged_in") {
-                            window.location.replace("index.html#login");
-                        }
-                    console.log("get OK");
-                    page = Mustache.render(template, data);
-                    $('#container').html(page);
-                });
+                $.getJSON(serverURL + "get.php?MODAL=" + sessionStorage['session_id'] + "&filter=all",
+                        function (data) {
+                            if (data === "not_logged_in") {
+                                window.location.replace("index.html#login");
+                            }
+                            console.log("get OK");
+                            page = Mustache.render(template, data);
+                            $('#container').html(page);
+                        });
             }, 'html');
             break;
     }
