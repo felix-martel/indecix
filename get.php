ffb0d4b7ba872e10c85ea86d6acb9910a7a54391
Der Siebte Schatten <der-siebte-schatten@D-850>
2016-10-21 14:51:49 +0200

    Implémentation du flag

--- get.php
+++ get.php
@@ -5,23 +5,25 @@ ini_set('session.use_trans_sid', 1);
 session_name('MODAL');
 session_start();
 
+header('Access-Control-Allow-Origin:*');
+
 require_once('database.php');
 
-header('Access-Control-Allow-Origin:*');
+
 
 function get_khotes($filter, $user_id) 
 {
 	$queries = array(
-	'all' => 	'SELECT khote_id, khoteur, khote, up, down, fav 
+	'all' => 	'SELECT khote_id, khoteur, khote, up, down, fav, flag 
 				FROM khote 
 				ORDER BY date DESC 
 				LIMIT 100',
-	'top' =>	'SELECT khote_id, khoteur, khote, up, down, fav 
+	'top' =>	'SELECT khote_id, khoteur, khote, up, down, fav, flag 
 				FROM khote 
 				WHERE up + down > 20 AND (down = 0 OR up/down > 2) 
 				ORDER BY up/(down+1) DESC 
 				LIMIT 100',
-	'fav' =>	'SELECT k.khote_id, k.khoteur, k.khote, k.up, k.down, k.fav 
+	'fav' =>	'SELECT k.khote_id, k.khoteur, k.khote, k.up, k.down, k.fav, k.flag 
 				FROM khote AS k
 				LEFT JOIN relation  AS r 
 				ON k.khote_id = r.khote_id 
