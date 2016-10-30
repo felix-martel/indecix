642a8f54f2ac9abb6d1e6894d96e3650c2573f67
Der Siebte Schatten <der-siebte-schatten@D-850>
2016-10-30 16:46:34 +0100

    Implémentation de l'envoi du courriel d'activation

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set('session.use_cookies', 0);
 ini_set('session.use_only_cookies', 0);
 ini_set('session.use_trans_sid', 1);
@@ -14,16 +15,16 @@ header('Access-Control-Allow-Origin:*');
 
 $msgJson = array();
 $statusMessage = array('status' => 'error', 'code' => 'default_error', 'detail' => 'Unknown error');
-function setStatus($success, $text){
+
+function setStatus($success, $text) {
     global $statusMessage;
-    if ($success){
+    if ($success) {
         $statusMessage['status'] = 'success';
         $statusMessage['code'] = '';
-    }
-    else {
+    } else {
         $statusMessage['status'] = 'error';
         $statusMessage['code'] = $text;
-        switch($text) {
+        switch ($text) {
             case 'empty_field':
                 $statusMessage['detail'] = 'Username or password is empty';
                 break;
@@ -35,19 +36,23 @@ function setStatus($success, $text){
                 break;
             case 'username_already_exists':
                 $statusMessage['detail'] = 'This username already exists. Please choose another one';
+                break;
             case 'email_already_exists':
                 $statusMessage['detail'] = 'This e-mail address is already associated with an account. Please use another one';
+                break;
+            case 'email_not_sent':
+                $statusMessage['detail'] = 'There was an issue sending the activation e-mail. Please verify your e-mail adress or contact the webmaster.';
+                break;
             default:
                 $statusMessage['detail'] = 'Unknown error';
                 break;
-
         }
     }
-    
 }
+
 // tableau pour la gestion des erreurs
 $msgJson = array();
-$serverURL = "";
+$serverURL = "http://s621682634.onlinehome.fr/";
 
 try {
     $username = $_POST['username'];
@@ -57,12 +62,10 @@ try {
     assert(!empty($username));
     assert(!empty($email));
     assert(strlen($password) > 5);
-}
-catch (Exception $e) {
+} catch (Exception $e) {
     setStatus(false, 'empty_field');
     //$msg = array('error' => 'empty_field');
-}
-catch(AssertException $e){
+} catch (AssertException $e) {
     setStatus(false, 'empty_field');
     //$msg = array('error' => 'empty_field');
 }
@@ -95,37 +98,32 @@ if (isset($_POST['username'], $_POST['password']) && !empty($_POST['username']) 
         //array_push($msgJson, $msg);
     }
     // Insertion de l'utilisateur
-    $verif_key = md5(microtime(TRUE)*100000);
-    $sth = $dbh->prepare("INSERT INTO user (name, mail, password, authkey) VALUES (:username, :email, SHA1(:password), :key)"); 
+    $verif_key = md5(microtime(TRUE) * 100000);
+    $sth = $dbh->prepare("INSERT INTO user (name, mail, password, authkey) VALUES (:username, :email, SHA1(:password), :key)");
     $sth->execute(array(
         'username' => $username,
-        'email' => $email, 
+        'email' => $email,
         'password' => $password,
         'key' => $verif_key
     ));
     setStatus(true, '');
+
     // Envoi du mail de confirmation
     $recipient = $email;
-    $title = "Activate your KHOTE account";
-    $header = "From: activation@khote.com";
-    $activation_link = $serverURL."auth.php?user=".urlencode($username).'&key='.urlencode($verif_key);
-    $login_link = $serverURL."index.html#login";
+    $title = 'Activate your KHOTE account';
+    $header = "MIME-Version: 1.0\r\n"; 
+    $header .= "From: Jacques Biot, CEO of KHOTES <activation@khote.com>\r\n";
+    $header .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $header .= "X-Mailer: PHP/".phpversion();
+    $activation_link = $serverURL . 'auth.php?user=' . urlencode($username) . '&key=' . urlencode($verif_key);
+    $login_link = $serverURL . 'index.html#login';
 
-    $message = 'Hi,
-     Thanks for signing up to KHOTE !
-
-     To activate your account, click on this link : '.$activation_link.'
-     After you have activated your account, you can login at '.$login_link.'
-
-     Best regards,
-     Jacques Biot, CEO of KHOTE
-
-     ---
-
-     Please do not reply, I\'m very busy right now';
-     // Serveur mail à configurer...
-     //mail($recipient, $title, $message, $header);
-
+    $message = "Hi,\r\nThanks for signing up to KHOTE !\r\n\r\nTo activate your account, click on this link : " . $activation_link . "\r\nAfter you have activated your account, you can login at " . $login_link . "\r\n\r\nBest regards,\r\nJacques Biot, CEO of KHOTE\r\n\r\n---\r\n\r\nPlease do not reply, I'm very busy right now";
+    if (mail($recipient, $title, $message, $header)) {
+        setStatus(true, '');
+    } else {
+        setStatus(false, 'email_not_sent');
+    }
 } else {
     $msg = array('error' => 'Login or password is not set');
 }
