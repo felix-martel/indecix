eb9e2c247c5e6a75b60e8e7787b8b0bad31938b8
Der Siebte Schatten <der-siebte-schatten@D-850>
2016-10-30 14:39:16 +0100

    Passage aux valeurs du serveur en ligne

--- database.php
+++ database.php
@@ -1,17 +1,18 @@
 <?php
 class Database {
 	public static function connect() {
+            /*
 		$dsn = 'mysql:dbname=indecix;host=127.0.0.1';
 		$user = 'root';
 		$pwd = ''; //modal
 		$dbh = null;
+             */
 
-		/*
+
 		$dsn  = "mysql:dbname=db646744700;host=db646744700.db.1and1.com";
-		$database   = "db646744700";
 		$user  = "dbo646744700";
 		$pwd   = "Khotes!X2015";
-		*/
+                $dbh = null;
 		
 		try {
 			$dbh = new PDO($dsn, $user, $pwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
